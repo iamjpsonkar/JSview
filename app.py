09b8bfca5d74f440ad5ac113c931300ea055bd84
Jay Prakash Sonkar <jsonkar@gofynd.com>
2023-03-22 20:25:33 +0530

    working app

--- app.py
+++ app.py
@@ -1,57 +1,48 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QTabWidget, QWidget, QVBoxLayout, QLabel, QPlainTextEdit
-import sys
-from PyQt5.Qt import QApplication, QClipboard
-from PyQt5 import QtCore, QtWidgets
-from PyQt5.QtWidgets import QMainWindow, QWidget, QPlainTextEdit
-from PyQt5.QtCore import QSize
+from PyQt5.QtWidgets import QTabWidget, QAction, QPlainTextEdit, QPushButton
+from PyQt5.Qt import QApplication
+from PyQt5 import QtCore
+from PyQt5.QtWidgets import QMainWindow, QPlainTextEdit, QTextEdit
+from PyQt5.QtWidgets import QMenu
+from PyQt5.QtWidgets import QShortcut
+from PyQt5.QtGui import QKeySequence
+import json
+
 
 class MyWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-
-        # Create a QTabWidget
+        
         self.tabs = QTabWidget()
+        # self.tabs.tabsClosable()
         self.setCentralWidget(self.tabs)
+        self.setWindowState(QtCore.Qt.WindowMaximized)
+
         
         self.cur_tab = 0
-        self.tab_list = [{}]
+        self.tab_list = []
+        self.newtabshortcut = QShortcut(QKeySequence("Ctrl+N"), self)
+        self.newtabshortcut.activated.connect(self.add_new_tab)
 
+
+        self.loadjsonshortcut = QShortcut(QKeySequence("Ctrl+L"), self)
+        self.loadjsonshortcut.activated.connect(self.process_json)
+
+        self.add_new_tab()
+        
+    def add_new_tab(self):
+        self.cur_tab = len(self.tab_list)
+        self.tab_list.append(QTextEdit(self))
+        self.tabs.addTab(self.tab_list[self.cur_tab], "New Tab")
+        self.tab_list[self.cur_tab].setAcceptRichText(True)
+        self.tab_list[self.cur_tab].autoFormatting()
         
 
-        # Create first Tab
-        self.tab_list[self.cur_tab]["tabName"] = QWidget()
-
-        
-        self.tabs.addTab(self.tab_list[self.cur_tab]["tabName"], "New Tab")
-
-        # Add widgets to the tabs
-        self.tab_list[self.cur_tab]["tabLayout"] = QVBoxLayout()
-
-        self.tab_list[self.cur_tab]["tabEditor"] = QPlainTextEdit(self.tab_list[self.cur_tab]["tabName"])
-
-        # Add text field
-        # self.b = QPlainTextEdit(self)
-        self.tab_list[self.cur_tab]["tabEditor"].insertPlainText("You can write text here.\n")
-        self.tab_list[self.cur_tab]["tabEditor"].move(10,10)
-        self.tab_list[self.cur_tab]["tabEditor"].resize(400,200)
-
-        self.tab_list[self.cur_tab]["tabLayout"].addWidget(self.tab_list[self.cur_tab]["tabEditor"])
-        
-
-        # self.tab1_label = QLabel("This is tab 1")
-        # self.tab1_layout.addWidget(self.tab1_label)
-        # self.tab1.setLayout(self.tab1_layout)
-
-        # self.tab2_layout = QVBoxLayout()
-        # self.tab2_label = QLabel("This is tab 2")
-        # self.tab2_layout.addWidget(self.tab2_label)
-        # self.tab2.setLayout(self.tab2_layout)
-
-        # self.tab3_layout = QVBoxLayout()
-        # self.tab3_label = QLabel("This is tab 3")
-        # self.tab3_layout.addWidget(self.tab3_label)
-        # self.tab3.setLayout(self.tab3_layout)
-
+    def process_json(self):
+        self.cur_tab = self.tabs.currentIndex()
+        text = self.tab_list[self.cur_tab].toPlainText()
+        self.tab_list[self.cur_tab].clear()
+        text = json.dumps(json.loads(text), sort_keys=True, indent=4)
+        self.tab_list[self.cur_tab].insertPlainText(text)
 
 
 
@@ -61,28 +52,3 @@ window = MyWindow()
 window.show()
 app.exec_()
 
-
-# import sys
-# from PyQt5.Qt import QApplication, QClipboard
-# from PyQt5 import QtCore, QtWidgets
-# from PyQt5.QtWidgets import QMainWindow, QWidget, QPlainTextEdit
-# from PyQt5.QtCore import QSize
-
-# class ExampleWindow(QMainWindow):
-#     def __init__(self):
-#         QMainWindow.__init__(self)
-
-#         self.setMinimumSize(QSize(440, 240))    
-#         self.setWindowTitle("PyQt5 Textarea example") 
-
-#         # Add text field
-#         self.b = QPlainTextEdit(self)
-#         self.b.insertPlainText("You can write text here.\n")
-#         self.b.move(10,10)
-#         self.b.resize(400,200)
-
-# if __name__ == "__main__":
-#     app = QtWidgets.QApplication(sys.argv)
-#     mainWin = ExampleWindow()
-#     mainWin.show()
-#     sys.exit( app.exec_() )
